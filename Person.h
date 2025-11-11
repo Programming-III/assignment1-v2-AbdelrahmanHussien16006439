b10c151442503171a1c5722c35328a38ad7bc7da
AbdelrahmanHussien16006439 <abdelrahmanihab22@gmail.com>
2025-11-11 09:56:55 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -5,7 +5,21 @@
 using namespace std;
 
 //#write Person class here
+class Person{
+    private:    
+    string name;    
+    int id;
+    public:
+    Person();
+    Person(string n, int i);
+    ~Person();
+    void setName(string n);
+    void setId(int i);
+    string getName() const;
+    int getId() const;
 
+    virtual void display() const;
+};
 
 
 
